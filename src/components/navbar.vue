1d3fac198ed7e513ab142e696cfa3496ec85be04
ahmadnajjar90 <a7mad.najjar@hotmail.com>
2020-09-06 01:56:56 +0200

    2

---
+++ src/components/navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <navbar1/>
+    <navbar2/>
+  </div>
+</template>
+
+<script>
+import navbar1 from './Header/navbar1'
+import navbar2 from './Header/navbar2'
+
+export default {
+    components:{
+    navbar1,
+    navbar2
+  }
+}
+</script>
+
+<style>
+
+</style>
