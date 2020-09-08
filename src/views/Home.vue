9ca44479b5878c24e65952ec650075de605543a4
ahmadnajjar90 <a7mad.najjar@hotmail.com>
2020-09-08 23:56:15 +0200

    4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="home">
+    <homeCarousel/>
+    <NewCustomerDiscount/>
     <subscribe/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import homeCarousel from '@/components/homeCarousel'
+import NewCustomerDiscount from '@/components/NewCustomerDiscount'
 import subscribe from '@/components/subscribe'
+
 export default {
   name: 'Home',
   components: {
-    subscribe,
+    homeCarousel,
+    NewCustomerDiscount,
+    subscribe
   }
 }
 </script>
